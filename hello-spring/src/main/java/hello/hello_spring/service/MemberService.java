9276e4ac487fb0f2ea92cedca02c51b9c956244c
Yelim Shin <yyyeee1108@gmail.com>
2024-06-28 19:05:15 +0900

    스프링 입문 - 컴포넌트 스캔과 자동 의존관계 설정
    MemberController 생성 - @Controller 애너테이션 사용, @Autowired를 이용 생성자 주입 방식으로 MemberService 주입
    MemberService - @Service 사용하여 스프링 빈으로 등록, @Autowired를 이용 생성자 주입 방식으로 MemberRepository 주입
    MemoryMemberRepository - @Repository 사용하여 스프링 빈으로 등록

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // Dependency Injection
+    @Autowired // 객체 생성 시점에 스프링 컨테이너에서 해당 스프링 빈을 찾아 주입
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
