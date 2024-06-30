be36e63e3bedbf73555d7ac56dcb93873004946a
Yelim Shin <yyyeee1108@gmail.com>
2024-06-30 22:17:58 +0900

    스프링 입문 - 스프링 통합 테스트
    @SpringBootTest, @Transactional 이용
    스프링까지 올려서 테스트한다
    트랜잭션, 커밋, 롤백 개념

--- hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -6,25 +6,20 @@ import hello.hello_spring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     void 회원가입() {
@@ -40,18 +35,17 @@ class MemberServiceTest {
     }
 
     @Test
-    public void 중복_회원_예외() {
-        // Given
+    public void 중복_회원_예외() throws Exception {
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
 
-        // When
+        //When
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> memberService.join(member2));
+                () -> memberService.join(member2));//예외가 발생해야 한다.
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 }
